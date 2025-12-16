74db5966b69ec554df5f4e8536b0d470ad4f8487
konradkelly <konradky@gmail.com>
2025-12-15 18:22:59 -0800

    add form validation for HTML contact form with email regex

---
+++ public/js/form_validation.php
@@ -0,0 +1,29 @@
+<?php
+$firstName = $email = $message = "";
+$firstNameErr = $emailErr = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Validate First Name
+    if (empty($_POST["firstName"])) {
+        $firstNameErr = "First name is required";
+    } else {
+        $firstName = htmlspecialchars($_POST["firstName"]);
+    }
+
+    // Validate Email with regex
+    if (empty($_POST["email"])) {
+        $emailErr = "Email is required";
+    } else {
+        $email = htmlspecialchars($_POST["email"]);
+        $pattern = "/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/";
+        if (!preg_match($pattern, $email)) {
+            $emailErr = "Invalid email format";
+        }
+    }
+
+    // Message is optional
+    if (!empty($_POST["message"])) {
+        $message = htmlspecialchars($_POST["message"]);
+    }
+}
+?>
